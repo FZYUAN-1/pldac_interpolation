6cf0d790e53e7bb20dd1c7762d1bec56df864111
ZhuangPascal <47885857+ZhuangPascal@users.noreply.github.com>
2021-02-18 09:44:20 +0100

    Add files via upload

--- model_physique_test.py
+++ model_physique_test.py
@@ -1,14 +1,19 @@
 # %%
+#Importation des librairies
 import model_physique as m_phy
 import methods as mtds
 import dataSource as ds
+import dataVisualization as dv
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 # %%
 #Récupération des données et des paramètres
 df = ds.importData()
 latitude_min, latitude_max, longitude_min, longitude_max, ecart_x, ecart_y = ds.calcul_param(df)
-pos = (0,1)
+pos = (4,4)
+n_interval = 10
 
 # %%
 # group by trip, convert to numpy matrix
@@ -29,7 +34,7 @@ m = m_phy.modele_physique()
 
 # %%
 #Fonctions splits
-def split_OneTrip(trip, st = 0.5, k1 = 1, k2 = 1, random = 1):
+def split_OneTrip(trip, st = 0.5, k1 = 0.1, k2 = 0.1, random = 1):
 	'''
 	trip : np.array [data points in one trip]
 	st : sample from first st% data_points
@@ -66,18 +71,16 @@ def split_AllTrip(trips, params = None):
 def test1(trips,func_cost):
 	mat_first, mat_last = split_AllTrip(trips)
 	cost = 0
-	cpt_boucle = 0
 	#print(mat_first, mat_last)
 	for i in range(len(mat_first)):
 		first, last = mat_first[i], mat_last[i]
 		#print(first, last)
 		# returns all combinations of tuple (A_n-1, A_n, A_n+1)
 		for elem_last in last:
-			for j in range(len(first)-1):
+			for j in range(len(first)-1):       
 				for k in range(j+1, len(first)):
 					cost += m.score(func_cost,np.array([[first[j], first[k], elem_last]]))
-					print(cpt_boucle)
-					cpt_boucle+=1
+
 	return cost
 
 #%%
@@ -100,17 +103,24 @@ def calcul_mat_err(df, latitude_min, longitude_min, ecart_x, ecart_y, n_interval
 				for t in case_df.groupby('Trip'):
 					mat.append(t[1][['Latitude','Longitude','GpsTime']].to_numpy())
 
-				mat_err[i,j] = test1(mat,mtds.moindre_c)
-				print(mat_err[i,j])
+				mat_err[n_interval-1-i,j] = test1(mat,mtds.moindre_c)
+				print(mat_err[n_interval-1-i,j])
 			
 	return mat_err
 	        
 				
 # %%
 #Test
-cost = test1(mat,mtds.moindre_c)
-print(cost)
 
-#print(calcul_mat_err(df, latitude_min, longitude_min, ecart_x, ecart_y))
+#cost = test1(mat,mtds.moindre_c)
+#print(cost)
+
+dv.affiche_carte(df, pos, latitude_min, latitude_max, longitude_min, longitude_max, ecart_x, ecart_y)
+#dv.afficher_hist_norm_vit(df, pos, latitude_min, longitude_min, ecart_x, ecart_y)
+
+mat_err = calcul_mat_err(df, latitude_min, longitude_min, ecart_x, ecart_y)
+plt.title("Matrice des erreurs")
+sns.heatmap(mat_err, linewidths=.5, cmap="YlGnBu", yticklabels = np.arange(n_interval-1,-1,-1))
+plt.show()
 
 # %%
