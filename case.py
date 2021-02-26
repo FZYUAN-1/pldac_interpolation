78643a1891eaf21beebfa56482b8aaabcbaf18f8
Bozhang Huang <thibaulthuang@gmail.com>
2021-02-26 16:08:59 +0100

    h

--- case.py
+++ case.py
@@ -17,32 +17,46 @@ dffer_de_w21 = sum_x_y (2 * longx * (latiy - p_latiy)) = 0
 dffer_de_w12 = sum_x_y (2 * latix * (longy - p_longy)) = 0
 dffer_de_w22 = sum_x_y (2 * longx * (longy - p_longy)) = 0
 """
+class RegLineaire2():
+    def __init__(self,datax,datay):
+        self.datax = datax
+        self.datay = datay
 
-def fit_case_ML(df, pos, latitude_min, longitude_min, ecart_x, ecart_y):
-    "Retourner le paramètre W appris dans la case indiquée."
-    df_case = ds.trouve_data_case(df, pos, latitude_min, longitude_min, ecart_x, ecart_y)
-    trips = np.unique(df['Trip'])
-    for t in trips:
-        df2 = df[df['Trip'] == t][['Latitude','Longitude']]
-        N = df2['Latitude'].shape[0]
-        ens_points = np.hstack((df2['Latitude'].values.reshape(N,1), df2['Longitude'].values.reshape(N,1)))
-        ens_x = ens_points[:-1]
-        ens_y = ens_points[1:]
+    def fit(self,df):
+        "Retourner le paramètre W appris dans la case indiquée."
+        #latitude_min, latitude_max, longitude_min, longitude_max, ecart_x, ecart_y = ds.calcul_param(df)
+        #df_case = ds.trouve_data_case(df, pos, latitude_min, longitude_min, ecart_x, ecart_y)
+        trips = np.unique(df['Trip'])
+
+        for t in trips:
+            df2 = df[df['Trip'] == t][['Latitude','Longitude']]
+            N = df2['Latitude'].shape[0]
+            ens_points = np.hstack((df2['Latitude'].values.reshape(N,1), df2['Longitude'].values.reshape(N,1)))
+            ens_x = ens_points[:-1]
+            ens_y = ens_points[1:]
+        
+        latix2 = sum(ens_x[:,0] ** 2)
+        latilongx = sum(ens_x[:,0] * ens_x[:,1])
+        longx2 = sum(ens_x[:,1] ** 2)
+        latixy = sum(ens_x[:,0] * ens_y[:,0])
+        latilongxy = sum(ens_x[:,0] * ens_y[:,1])
+        longxy = sum(ens_x[:,1] * ens_y[:,1])
+        longlatixy = sum(ens_x[:,1] * ens_y[:,0])
+
+
+
+        w1 = np.linalg.solve(np.array([[latix2, latilongx],[latilongx, longx2]]), np.array([2*latixy, 2*longlatixy]))
+        w2 = np.linalg.solve(np.array([[latix2, latilongx],[latilongx, longx2]]), np.array([2*latilongxy, 2*longxy]))
+        
+        self.coef_ = np.vstack((w1, w2))
     
-    latix2 = sum(ens_x[:,0] ** 2)
-    latilongx = sum(ens_x[:,0] * ens_x[:,1])
-    longx2 = sum(ens_x[:,1] ** 2)
-    latixy = sum(ens_x[:,0] * ens_y[:,0])
-    latilongxy = sum(ens_x[:,0] * ens_y[:,1])
-    longxy = sum(ens_x[:,1] * ens_y[:,1])
-    longlatixy = sum(ens_x[:,1] * ens_y[:,0])
-
-
-
-    w1 = np.linalg.solve(np.array([[latix2, latilongx],[latilongx, longx2]]), np.array([2*latixy, 2*longlatixy]))
-    w2 = np.linalg.solve(np.array([[latix2, latilongx],[latilongx, longx2]]), np.array([2*latilongxy, 2*longxy]))
+    def predict(self,datax):
+        return datax.dot(self.coef_)
     
-    return np.vstack((w1, w2))
+    def score(self,datax,datay):
+        datay_p = self.predict(datax)
+        
+        return ((datay_p - datay)**2).sum()
 
 df = ds.importData()
 
