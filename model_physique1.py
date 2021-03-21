0322afef99b5ab7e3be8e47539e85d07ede1b9ea
YUAN <intmeursault@gmail.com>
2021-03-21 20:11:14 +0100

    yuan edit

--- model_physique1.py
+++ model_physique1.py
@@ -1,82 +1,42 @@
-import model_abstract as baseM
+from sklearn.base import BaseEstimator, RegressorMixin
 import numpy as np
 
 
 #### Modele Physique
-class model_physique1(baseM.base_model):
+class model_physique1(BaseEstimator,RegressorMixin):
 
-    def __init__(self,freq,eval):
-        self.freq = freq
-        self.strategys = [self.strategy_avg,self.strategy_last2]
-        self.eval = eval
+    def __init__(self, step_test):
+        super().__init__()
+        self.step_test = step_test
 
-    def fit(self, tr):
+    def fit(self, X, y):
+        return self
+
+    def predict(self,X):
         '''
-        param : tr : dataframe in one cell
-                self.x_train : [[[lat1,longi1,GpsTime1],[lat2,longi2,GpsTime2],...,[latN,longiN,GpsTime2]]*N_trip]
-        '''
-        mat = []
-        groups = tr.groupby('Trip')
-        for t in groups:
-            trip_i = t[1][['Latitude', 'Longitude', 'GpsTime']].to_numpy()
-            # take continuous points, step = freq//200
-            tmp = []
-            for i in range(0,len(trip_i),int(self.freq//200)):
-                tmp.append(trip_i[i])
-
-            mat.append(tmp)
-
-        mat = np.array(mat)
-        
-        self.x_train = np.array(mat)
-    
-    def predictOne(self, x_test, alpha):
-        '''
-        param: x_test : [[prevlat,prevlongi,GpsTime],[lat,longi,GpsTime]]
-        retourne  [nextlat, nextlongi] : prochaine lat et longi apres la duree alpha
-        '''
-        print(x_test)
-        if x_test[1][2] == x_test[0][2]:
-            #print(x_test)
-            raise ValueError("GpsTime Equal Error")
-
-        v_speed = (x_test[0][:2] - x_test[1][:2])/(x_test[1][2] - x_test[0][2])
-        return x_test[1][:2] + v_speed*alpha
-
-    def predict(self,alpha):
-        '''
-        param: [alpha1, alpha2,...,] <=> GpsTime
-        retourne  [[nextlat, nextlongi]*N_trip] : prochaine lat et longi apres la duree alpha
+        param [[Trip,Latitude,Longitude,GpsTime]*m]
+        retourne  [[nextlat, nextlongi]*m] : prochaine lat et longi
         ''' 
-        M = len(self.strategys)
-        N = len(self.x_train)
-        d = np.zeros((N,M,2))
-        for i in range(N):
-            for j in range(len(self.strategys)):
-                d[i,j] = self.strategys[j](self.x_train[i], alpha)
-        return d
-
-    def score(self,x_test):
-        '''
-        param: x_test : [[[prevlat,prevlongi,GpsTime],[lat,longi,GpsTime],[nextlat,nextlongi,GpsTime]]*N]
-               func : fonction d'evaluation, i.e. moindre_c
-
-               x_test : [[nextlat,nextlongi,GpsTime]*N_trip]
-        '''
         
-        # alpha = GpsTime_A_n+1 - GpsTime_A_n
-        alpha =  x_test[:,2] - self.x_train[:,-1]
-        X_predit = self.predict(alpha)
-        X_true = x_test[:,:2]
-        return self.eval(X_predit,X_true)
-    
-    def strategy_last2(self,x_train_i, alpha):
-    # prendre les 2 derniers points precedents
-        return self.predictOne([x_train_i[-2],x_train_i[-1]], alpha)
+        mat = []
+        groups = X.groupby('Trip')
+        for group in groups:
+            mat.append(group[1][['Latitude','Longitude','GpsTime']].to_numpy())
+        
+        #print(mat)
+        try:
+            res = []
+            train_step = mat[0][1][2] - mat[0][0][2]
+        except IndexError:
+            print('indexerror',X, mat)
+        #print(train_step)
 
-    def strategy_avg(self,x_train_i, alpha):
-        # prendre les moyennes des positions predictes
-        return np.mean([self.predictOne([[x_train_i[m], x_train_i[n]]], alpha) for m in range(len(x_train_i)) for n in range(len(x_train_i)-1)])
+        for X_t in mat:
+            res.append(X_t[0][:2])
 
-    def strategy_rand2(self,x_train_i, alpha):
-        pass
+            for i in range(1,len(X_t)):
+                v = np.array([ (X_t[i-1][0] - X_t[i][0]) / train_step , (X_t[i-1][1] - X_t[i][1])/ train_step ])
+                res.append(X_t[i][:2] + self.step_test*v)
+        
+        #print(len(res), len(res[0]))
+        return np.array(res)
